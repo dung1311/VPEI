e0bd607fbd4ff853a3264cf8f1f4710dfb938388
dung1311 <tiendung13112004@gmail.com>
2026-04-07 10:39:09 +0700

    fix keyboard interrupt

--- run.py
+++ run.py
@@ -9,4 +9,7 @@ if __name__ == "__main__":
     print("🔧 Initialising database …")
     init_db()
     print("🚀 Starting VPEI on http://0.0.0.0:8000")
-    uvicorn.run("main:app", host="127.0.0.1", port=8000)
+    try:
+        uvicorn.run("main:app", host="127.0.0.1", port=8000)
+    except KeyboardInterrupt:
+        print("Đã đóng web")
