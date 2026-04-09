0b997fd4c735a1861360cb2f7d52f7468bdea38f
dung1311 <tiendung13112004@gmail.com>
2026-04-09 13:02:12 +0700

    update JSONResponse

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import JSONResponse
 from starlette.exceptions import HTTPException as StarletteHTTPException
 from core.middleware import SessionValidationMiddleware
 from routers import auth, dashboard, admin, scope1, scope2, scope3, settings, common, reports
